730deb1f72438ad7b0b8f408b14b939aa73ad602
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-10-26 14:17:53 +0200

    Make regexp for all fields

---
+++ index.js
@@ -0,0 +1,12 @@
+let nameRegex = /[aA-zZ]{1,}/g;
+let countryRegex = /[aA-zZ]{1,}/g;
+let cityRegex = /[aA-zZ]{1,}/g;
+let addressRegex = /[aA-zZ 1-9]{1,}/g;
+let zipRegex = /[A-Z1-9]{1,}/g;
+let cardNumberRegex = /[1-9]{4,4}-[1-9]{4,4}-[1-9]{4,4}-[1-9]{4,4}/g;
+let ccvRegex=/[1-9]{3}/g;
+let cardNameRegex = / [aA-zZ ]{1,}/g;
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+})
